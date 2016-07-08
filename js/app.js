4ca4343a6cef451f71ef5307c7b4204418fd84ab
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2016-07-08 16:21:15 +0200

    Fixed: _.showCompatibilityMessage in a private scope.

--- js/app.js
+++ js/app.js
@@ -5,24 +5,22 @@
 
 'use strict';
 
-( function() {
-	// Impromptu namespace for the sample.
-	var _ = {
-		showCompatibilityMessage: function() {
-			var editorElement = document.querySelector( '#editor' );
-			var message = document.createElement( 'p' );
+// Impromptu namespace for the sample.
+var _ = {
+	showCompatibilityMessage: function() {
+		var editorElement = document.querySelector( '#editor' );
+		var message = document.createElement( 'p' );
 
-			message.innerHTML = '\
-				<h2>That\'s a shame...</h2>\
-				<p>We\'re not proud of this but this early developer preview does not work in your web browser.</p>\
-				<p>Please consider using <a href="https://www.google.com/chrome/">Google Chrome</a> instead.</p>\
-				<p>We\'re terribly sorry.</p>\
-			';
+		message.innerHTML = '\
+			<h2>That\'s a shame...</h2>\
+			<p>We\'re not proud of this but this early developer preview does not work in your web browser.</p>\
+			<p>Please consider using <a href="https://www.google.com/chrome/">Google Chrome</a> instead.</p>\
+			<p>We\'re terribly sorry.</p>\
+		';
 
-			message.classList.add( 'message' );
+		message.classList.add( 'message' );
 
-			editorElement.style.display = 'none';
-			editorElement.parentNode.insertBefore( message, editorElement );
-		}
-	};
-} );
+		editorElement.style.display = 'none';
+		editorElement.parentNode.insertBefore( message, editorElement );
+	}
+};
