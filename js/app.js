8a86d9e69faa53cb981229c34bcd48e5f68ac29b
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2016-07-08 14:53:55 +0200

    Code refactorin in JS file.

--- js/app.js
+++ js/app.js
@@ -5,24 +5,29 @@
 
 'use strict';
 
-var isCompatible = navigator.userAgent.toLowerCase().indexOf( 'chrome' ) != -1;
+// Impromptu namespace for the sample.
+var _ = {
+	isCompatible: navigator.userAgent.toLowerCase().indexOf( 'chrome' ) != -1
+};
 
-if ( !isCompatible ) {
-	// Display the message of shame.
-	document.addEventListener( 'DOMContentLoaded', function() {
-		var editorElement = document.querySelector( '#editor' );
-		var message = document.createElement( 'p' );
+( function() {
+	if ( !_.isCompatible ) {
+		// Display the message of shame.
+		document.addEventListener( 'DOMContentLoaded', function() {
+			var editorElement = document.querySelector( '#editor' );
+			var message = document.createElement( 'p' );
 
-		message.innerHTML = [
-			'<h2>That\'s a shame...</h2>',
-			'<p>We\'re not proud of this but this early developer preview of the editor<br/> ',
-			'works in <a href="https://www.google.com/chrome/">Google Chrome</a> only.</p>',
-			'<p>We\'re terribly sorry.</p>'
-		].join( '' );
+			message.innerHTML = '\
+				<h2>That\'s a shame...</h2>\
+				<p>We\'re not proud of this but this early developer preview of the editor<br/>\
+				works in <a href="https://www.google.com/chrome/">Google Chrome</a> only.</p>\
+				<p>We\'re terribly sorry.</p>\
+			';
 
-		message.classList.add( 'message' );
+			message.classList.add( 'message' );
 
-		editorElement.style.display = 'none';
-		editorElement.parentNode.insertBefore( message, editorElement );
-	} );
-}
+			editorElement.style.display = 'none';
+			editorElement.parentNode.insertBefore( message, editorElement );
+		} );
+	}
+} )();
