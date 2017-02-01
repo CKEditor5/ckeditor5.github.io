8a41f67d7912455793d351519b6172f84587d150
Piotrek Koszuliński <pkoszulinski@gmail.com>
2017-02-01 15:18:01 +0100

    Updated license headers.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 /**
- * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md.
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
  */
 
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classic';
