b391c46c6205a53d14c70625077391e9d6e337bd
CircleCI <circleci@ip-10-0-125-2.ec2.internal>
2024-07-10 02:20:06 +0000

    Documentation build.

---
+++ misc/ime-snapshot-220610/ckeditor5/34.1.0/snippets/features/table-caption/snippet.js
@@ -0,0 +1,36 @@
+/*!
+ * @license Copyright (c) 2003-2022, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./node_modules/umberto/source/ckeditor5/34.1.0/_snippets/features/table-caption.js":
+/*!******************************************************************************************!*\
+  !*** ./node_modules/umberto/source/ckeditor5/34.1.0/_snippets/features/table-caption.js ***!
+  \******************************************************************************************/
+/***/ (() => {
+
+eval("/**\n * @license Copyright (c) 2003-2022, CKSource Holding sp. z o.o. All rights reserved.\n * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license\n */\n\n/* globals ClassicEditor, CKEditorPlugins, console, window, document */\n\nClassicEditor\n\t.create( document.querySelector( '#snippet-table-caption' ), {\n\t\textraPlugins: [\n\t\t\tCKEditorPlugins.TableCaption, CKEditorPlugins.Superscript\n\t\t],\n\t\ttable: {\n\t\t\tcontentToolbar: [ 'toggleTableCaption', '|', 'tableColumn', 'tableRow', 'mergeTableCells' ]\n\t\t},\n\t\timage: {\n\t\t\ttoolbar: [\n\t\t\t\t'imageStyle:block',\n\t\t\t\t'imageStyle:side',\n\t\t\t\t'|',\n\t\t\t\t'imageTextAlternative'\n\t\t\t]\n\t\t},\n\t\tui: {\n\t\t\tviewportOffset: {\n\t\t\t\ttop: window.getViewportTopOffsetConfig()\n\t\t\t}\n\t\t}\n\t} )\n\t.then( editor => {\n\t\twindow.editorCaption = editor;\n\t} )\n\t.catch( err => {\n\t\tconsole.error( err.stack );\n\t} );\n\n\n//# sourceURL=webpack://ckeditor5/./node_modules/umberto/source/ckeditor5/34.1.0/_snippets/features/table-caption.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./node_modules/umberto/source/ckeditor5/34.1.0/_snippets/features/table-caption.js"]();
+/******/ 	
+/******/ })()
+;
