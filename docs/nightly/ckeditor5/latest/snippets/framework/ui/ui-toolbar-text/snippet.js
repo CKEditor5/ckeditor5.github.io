f3de04a61a29cf5d12f88e53c6e37843fe2812b8
CircleCI <circleci@ip-10-0-41-235.ec2.internal>
2024-10-23 01:23:05 +0000

    Documentation build.

--- docs/nightly/ckeditor5/latest/snippets/framework/ui/ui-toolbar-text/snippet.js
+++ docs/nightly/ckeditor5/latest/snippets/framework/ui/ui-toolbar-text/snippet.js
@@ -1,5 +1,5 @@
 /*!
  * @license Copyright (c) 2003-2024, CKSource Holding sp. z o.o. All rights reserved.
  * For licensing, see LICENSE.md.
- */(()=>{const e=new Locale,t=new View;t.element=document.createTextNode("Toolbar text");const o=new ToolbarView(e);o.items.add(t),o.render(),document.querySelector(".ui-toolbar-text").append(o.element)})();
+ */(()=>{const e=new Locale,n=new View;n.element=document.createElement("span"),n.element.innerHTML="Toolbar text";const t=new ToolbarView(e);t.items.add(n),t.render(),document.querySelector(".ui-toolbar-text").append(t.element)})();
 //# sourceMappingURL=snippet.js.map
