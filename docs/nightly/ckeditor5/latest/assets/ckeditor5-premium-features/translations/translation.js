6359d0734154bd760bec985b3735ae02a72a1c1f
CircleCI <circleci@ip-10-0-46-62.ec2.internal>
2025-06-28 02:00:11 +0000

    Documentation build.

---
+++ docs/nightly/ckeditor5/latest/assets/ckeditor5-premium-features/translations/translation.js
@@ -0,0 +1,23 @@
+/*
+ *             Copyright (c) 2003-2025, CKSource Holding sp. z o.o. All rights reserved.
+ *
+ *
+ *
+ *
+ *          +---------------------------------------------------------------------------------+
+ *          |                                                                                 |
+ *          |                                 Hello stranger!                                 |
+ *          |                                                                                 |
+ *          |                                                                                 |
+ *          |   What you're currently looking at is the source code of a legally protected,   |
+ *          |    proprietary software. Any attempts to deobfuscate / disassemble this code    |
+ *          |               are forbidden and will result in legal consequences.              |
+ *          |                                                                                 |
+ *          |                                                                                 |
+ *          +---------------------------------------------------------------------------------+
+ *
+ *
+ *
+ *
+ */
+function _0x325a(_0x2b5f9d,_0x20c7e6){var _0x3dc0dd=_0x3dc0();return _0x325a=function(_0x325aa5,_0x47cfd5){_0x325aa5=_0x325aa5-0xb0;var _0x1b0f6c=_0x3dc0dd[_0x325aa5];return _0x1b0f6c;},_0x325a(_0x2b5f9d,_0x20c7e6);}(function(_0x2d4e36,_0x404610){var _0x568bd6=_0x325a,_0xf73ee2=_0x2d4e36();while(!![]){try{var _0x18064c=parseInt(_0x568bd6(0xb4))/0x1+-parseInt(_0x568bd6(0xb7))/0x2*(parseInt(_0x568bd6(0xb0))/0x3)+parseInt(_0x568bd6(0xb9))/0x4+parseInt(_0x568bd6(0xb5))/0x5+parseInt(_0x568bd6(0xb6))/0x6*(parseInt(_0x568bd6(0xb1))/0x7)+parseInt(_0x568bd6(0xb3))/0x8+parseInt(_0x568bd6(0xb8))/0x9*(-parseInt(_0x568bd6(0xb2))/0xa);if(_0x18064c===_0x404610)break;else _0xf73ee2['push'](_0xf73ee2['shift']());}catch(_0x2d3a55){_0xf73ee2['push'](_0xf73ee2['shift']());}}}(_0x3dc0,0x2ed84));function _0x3dc0(){var _0x22f2af=['6857073poFhzh','1368252KrxNaR','3PDotPY','32305JMrUVN','10FCIoVA','1893952vCToOo','236493JPctOy','694245osxOSr','258GIEOlW','397642uDuBOr'];_0x3dc0=function(){return _0x22f2af;};return _0x3dc0();}export default{'translation':{'dictionary':{},'getPluralForm':null}};
